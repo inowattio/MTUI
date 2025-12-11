80feb392692986740a5f98494e27d432083bbda9
CosminPerRam <cosmin.p@live.com>
2025-12-12 01:08:42 +0200

    add good num ops

--- src/num_ops.rs
+++ src/num_ops.rs
@@ -1,60 +1,54 @@
-use std::ops::{Add, Div, Mul, Neg};
+use num_traits::{CheckedAdd, CheckedDiv, CheckedMul, CheckedNeg};
 
 pub fn negate_opt_option<T>(v: &mut Option<T>)
 where
-    T: Neg<Output = T>,
+    T: CheckedNeg,
 {
-    *v = v.take().map(|n| -n);
+    *v = v.take().and_then(|n| n.checked_neg());
 }
 
 pub fn digit_remove_option<T>(v: &mut Option<T>)
 where
-    T: Copy + PartialEq + From<u8> + Div<Output = T>,
+    T: Copy + PartialEq + From<u8> + CheckedDiv<Output = T>,
 {
-    *v = v.and_then(|n| {
-        let ten = T::from(10u8);
-        let zero = T::from(0u8);
-        let divided = n / ten;
-
-        if divided == zero {
-            None
-        } else {
-            Some(divided)
-        }
-    })
+    *v = v.map(|n| {
+        let mut c = n;
+        digit_remove(&mut c);
+        c
+    });
 }
 
 pub fn digit_remove<T>(v: &mut T)
 where
-    T: Copy + PartialEq + From<u8> + Div<Output = T>,
+    T: Copy + PartialEq + From<u8> + CheckedDiv<Output = T>,
 {
     let ten = T::from(10u8);
-    *v = *v / ten
+    if let Some(c) = v.checked_div(&ten) {
+        *v = c;
+    }
 }
 
 pub fn digit_add_option<T>(v: &mut Option<T>, digit: u8)
 where
-    T: Copy + From<u8> + Add<Output = T> + Mul<Output = T>,
+    T: Copy + From<u8> + CheckedAdd<Output = T> + CheckedMul<Output = T>,
 {
-    assert!(digit < 10, "digit must be in 0..=9");
-
-    let ten = T::from(10u8);
-    let d = T::from(digit);
-
-    *v = Some(match *v {
-        Some(n) => n * ten + d,
-        None => d,
+    *v = v.map(|n| {
+        let mut c = n;
+        digit_add(&mut c, digit);
+        c
     });
 }
 
 pub fn digit_add<T>(v: &mut T, digit: u8)
 where
-    T: Copy + From<u8> + Add<Output = T> + Mul<Output = T>,
+    T: Copy + From<u8> + CheckedAdd<Output = T> + CheckedMul<Output = T>,
 {
     assert!(digit < 10, "digit must be in 0..=9");
 
     let ten = T::from(10u8);
     let d = T::from(digit);
 
-    *v = *v * ten + d;
+    if let Some(c) = v.checked_mul(&ten).and_then(|n| n.checked_add(&d)) {
+        *v = c;
+    }
 }
